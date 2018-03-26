c239f25f418fd5b682f35194b1f4d820cd0b7709
unknown <trendafil_gechev@abv.bg>
2018-03-26 12:28:45 +0200

    code with suggested changes: Made one TakeTimeMic function instead of having three, updated variable names etc.

--- ArduinoTimeMicDistance.lnk.ino
+++ ArduinoTimeMicDistance.lnk.ino
@@ -1,199 +1,207 @@
 #include <Servo.h>
-unsigned long time = micros();
+//unsigned long time = micros();
 
-float TimeMic1;
-float TimeMic2;
-float TimeMic3;
+float TimeMicA0;
+float TimeMicA4;
+float TimeMicA5;
 float First;
 float Second;
 
-int MicPin = A0;
-int MicValue1 = 0;
-int MicValue2 = 0;
+int MicА0 = A0;
+int MicA0Val1 = 0;
+int MicA0Val2 = 0;
 
-boolean Hit1=false;
-boolean Hit2=false;
-boolean Hit3=false;
+int MicA4 = A4;
+int MicA4Val1 = 0;
+int MicA4Val2 = 0;
 
+int MicA5 = A5;
+int MicA5Val1=0;
+int MicA5Val2=0;
 
-int MicPin1 = A4;
-int MicVal1 = 0;
-int MicVal2 = 0;
-
-int MicPin2 = A5;
-int MicPin2Val1=0;
-int MicPin2Val2=0;
-
+boolean HitMicA0 = false;
+boolean HitMicA4 = false;
+boolean HitMicA5 = false;
 
 Servo Servo1;
 
-
 void setup() {
-
-
  // Servo1.attach(servoPin);
-  pinMode(MicPin, INPUT);
-  pinMode(MicPin1, INPUT);
-  pinMode(MicPin2, INPUT);
+  pinMode(MicA0, INPUT);
+  pinMode(MicA4, INPUT);
+  pinMode(MicA5, INPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
 
-  MicValue1 = analogRead(MicPin);
-  MicValue2 = analogRead(MicPin);
-  MicVal1 = analogRead(MicPin1);
-  MicVal2 = analogRead(MicPin1);
-  MicPin2Val1=analogRead(MicPin2);
-  MicPin2Val2=analogRead(MicPin2);
+  MicA0Val1 = analogRead(MicA0);
+  MicA0Val2 = analogRead(MicA0);
+  MicA4Val1 = analogRead(MicA4);
+  MicA4Val2 = analogRead(MicA4);
+  MicA5Val1 = analogRead(MicA5);
+  MicA5Val2 = analogRead(MicA5);
 
-  //Serial.println(MicValue1);
-  //Serial.println(MicVal1);
-  //Serial.println(MicPin2Val1);
+  //Serial.println(MicA0Val1);
+  //Serial.println(MicA4Val1);
+  //Serial.println(MicA5Val1);
   
   Ready();
-
-
+  
   //Serial.println("first was ");
   //Serial.println(First);
 
 
-  //Serial.println(TimeMic1);
-  //Serial.println(TimeMic2);
+  //Serial.println(TimeMicA0);
+  //Serial.println(TimeMicA4);
 
 }
 
 void Ready() {
 
-  if (MicValue2 - MicValue1 > 20 && Hit1 == false ) {
-    TakeTimeMic1();
+  if (MicA0Val2 - MicA0Val1 > 20 && HitMicA0 == false ) {
+    TakeTimeMic(MicA0);
     //Serial.print("mic1=");
-    //Serial.println(TimeMic1);
+    //Serial.println(TimeMicA0);
      //delay(10);
   }
-  if (MicVal2 - MicVal1 > 20  && Hit2 == false ) {
-    
-    TakeTimeMic2();
+  if (MicA4Val2 - MicA4Val1 > 20  && HitMicA4 == false ) {
+	  
+    TakeTimeMic(MicA4);
     //Serial.print("mic2=");
-    //Serial.println(TimeMic2);
+    //Serial.println(TimeMicA4);
    // delay(10);
  
   } 
-  if (MicPin2Val2 - MicPin2Val1 > 20 && Hit3==false) {
+  if (MicA5Val2 - MicA5Val1 > 20 && HitMicA5==false) {
     
-    TakeTimeMic3();
+    TakeTimeMic(MicA5);
     //Serial.print("mic2=");
-    //Serial.println(TimeMic2);
+    //Serial.println(TimeMicA4);
     //delay(10);
   }
-  while (Hit1 == true && Hit2 == true && Hit3==true) {
-    Compare();
-  Hit1 = false;
-  Hit2 = false;
-  Hit3 = false;
-  delay(1000);
-  break;
-    }
   
-
+  while (HitMicA0 == true && HitMicA4 == true && HitMicA5==true) {
+    Compare();
+	HitMicA0 = false;
+	HitMicA4 = false;
+	HitMicA5 = false;
+	delay(1000);
+	break;
+  }
 }
 
 void Compare() {
-  if (TimeMic1 < TimeMic2 && TimeMic1 <TimeMic3) {
-    TimeMic1 = First;
+  if (TimeMicA0 < TimeMicA4 && TimeMicA0 <TimeMicA5) {
+    First = TimeMicA0;
     Serial.println("first was mic1");
     Servo1.write(0);
    //delay(1000);
 
   } 
-  if (TimeMic2 < TimeMic1 && TimeMic2 < TimeMic3) {
-    TimeMic2 = First;
+  if (TimeMicA4 < TimeMicA0 && TimeMicA4 < TimeMicA5) {
+    First = TimeMicA4;
     Serial.println("first was mic2");
     Servo1.write(180);
   // delay(1000);
   }
-  if ( TimeMic3< TimeMic1 && TimeMic3 < TimeMic2) {
-    TimeMic3 = First;
+  if ( TimeMicA5< TimeMicA0 && TimeMicA5 < TimeMicA4) {
+    First = TimeMicA5;
     Serial.println("first was mic3");
     Servo1.write(180);
   // delay(1000);
   }
 
   Servo1.write(90);
-  TimeMic1=0;
-  
-  TimeMic2=0;
-  TimeMic3=0;
+  TimeMicA0=0;
+  TimeMicA4=0;
+  TimeMicA5=0;
 
 }
 
+void TakeTimeMic(int mic){
+	switch(mic){
+		case MicA0:
+			TimeMicA0 = micros();
+			Serial.print("mic1=");
+			Serial.println(TimeMicA0);
+			HitMicA0 = true;
+		break;
+		case MicA4:
+			TimeMicA4 = micros();
+			Serial.print("mic2=");
+			Serial.println(TimeMicA4);
+			// Serial.print(TimeMicA4);
+			HitMicA4 = true;
+		break;
+		case MicA5:
+			TimeMicA5 = micros();
+			Serial.print("mic3=");
+			Serial.println(TimeMicA5);
+			// Serial.print(TimeMicA4);
+			HitMicA5 = true;
+		break;
+	}
+}
 
-void TakeTimeMic1() {
-  unsigned long time = micros();
-  TimeMic1 = time;
+/*
+void TakeTimeMicA0() {
+  TimeMicA0 = micros();
   Serial.print("mic1=");
-  Serial.println(TimeMic1);
-
-  Hit1 = true;
-  //return TimeMic1;
+  Serial.println(TimeMicA0);
+  HitMicA0 = true;
+  //return TimeMicA0;
 }
 
-void TakeTimeMic2( ) {
-  unsigned long time = micros();
-  TimeMic2 = time;
+void TakeTimeMicA4( ) {
+  TimeMicA4 = micros();
   Serial.print("mic2=");
-  Serial.println(TimeMic2);
-
-  // Serial.print(TimeMic2);
-
-   Hit2 = true;
-
+  Serial.println(TimeMicA4);
+  // Serial.print(TimeMicA4);
+   HitMicA4 = true;
 }
 
-void TakeTimeMic3( ) {
-  unsigned long time = micros();
-  TimeMic3 = time;
+void TakeTimeMicA5( ) {
+  TimeMicA5 = micros();
   Serial.print("mic3=");
-  Serial.println(TimeMic3);
-
-  // Serial.print(TimeMic2);
-
-   Hit3 = true;
-
+  Serial.println(TimeMicA5);
+  // Serial.print(TimeMicA4);
+   HitMicA5 = true;
 }
+*/
 
-/*  Serial.println(MicValue1);
+/*  Serial.println(MicA0Val1);
   Serial.print(" , ");
-  Serial.println(MicVal1);
+  Serial.println(MicA4Val1);
   // delay(10);
 */
-//Serial.println(MicValue2);
+//Serial.println(MicA0Val2);
 //Servo1.write(90);
 // delay(400);
-/*   if (MicVal2-MicVal1>10 && Hit1 == false){
+/*   if (MicA4Val2-MicA4Val1>10 && HitMicA0 == false){
 
 
   Servo1.write(180);
 
-  TimeMic1=time;
-  Serial.print(TimeMic1);
+  TimeMicA0=time;
+  Serial.print(TimeMicA0);
 
   Serial.print(" , ");
   Serial.println("moved left, ");
-  Hit1=true;
+  HitMicA0=true;
 
 
 
-    }else if (MicValue2-MicValue1>10 && Hit2 == false){
+    }else if (MicA0Val2-MicA0Val1>10 && HitMicA4 == false){
 
   Servo1.write(0);
   time = micros();
-  TimeMic1=time;
-  Serial.print(TimeMic1);
+  TimeMicA0=time;
+  Serial.print(TimeMicA0);
   Serial.print(" , ");
   Serial.println("moved right, ");
-  Hit2=true;
+  HitMicA4=true;
 
 
     }
